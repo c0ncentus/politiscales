5ef06ab6e575a2ee4c3b76b831f4131a505b3746
c0ncentus <jeremymartin33610@gmail.com>
2024-12-09 09:39:27 +0100

    up

--- partials/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer">
-  <p data-i18n="[html]footer">
-    <?= $i18n->get("[html]footer") ?>
-  </p>
-  <div class="darkmode-btn">
-    <input type="checkbox">
-    <span></span>
-  </div>
-</div>
---
+++ religionscales/src/app/lib.ts
@@ -0,0 +1,159 @@
+
+jQuery.loadScript = function (url, callback) {
+    jQuery.ajax({
+        url: url,
+        dataType: 'script',
+        success: callback,
+        async: false
+    });
+}
+
+$.loadScript('./langs/' + language + '/questions.js', function(){
+  var now = new Date();
+  var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  var timestamp = startOfDay / 1000;
+
+  // We shuffle questions once they have been dl with a different seed every day
+  shuffle(questions, timestamp);
+});
+
+start_time();
+
+function init_quiz() {
+  qn = 0; // Question number
+  prev_answer = null;
+
+  init_question();
+
+}
+
+// Count time passed on the quiz
+function start_time() {
+  startTime = new Date();
+};
+
+function end_time() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  timeDiff /= 1000;
+
+  // get seconds
+  var seconds = Math.round(timeDiff);
+  return(seconds);
+}
+
+function shuffle(array, seed) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  seed = seed || 1;
+  let random = function() {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+  };
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function init_question() {
+  question_string = $.i18n("question")
+  ques_of_string = $.i18n("of")
+
+  document.getElementById("question-text").innerHTML = questions[qn].question;
+  document.getElementById(
+    "question-number"
+  ).innerHTML = question_string +" "+ (qn + 1) +" "+  ques_of_string +" "+ questions.length
+
+  if (qn == 0) {
+    document.getElementById("back_button").style.display = "none";
+    document.getElementById("back_button_off").style.display = "block";
+  } else {
+    document.getElementById("back_button").style.display = "block";
+    document.getElementById("back_button_off").style.display = "none";
+  }
+}
+
+function next_question(mult) {
+  questions[qn].answer = mult;
+  qn++;
+
+  if (qn < questions.length) {
+    init_question();
+  } else {
+    results();
+  }
+}
+function prev_question() {
+  if (qn == 0) {
+    return;
+  }
+  qn--;
+  init_question();
+}
+
+function calc_score(score, max_value) {
+  return ((100 * score) / max_value).toFixed(0);
+}
+
+function results() {
+  var axes = {};
+
+  for (var i = 0; i < questions.length; i++) {
+    var q = questions[i];
+
+    for (var j = 0; j < q.valuesYes.length; j++) {
+      var a = q.valuesYes[j];
+      if (!(a.axis in axes)) {
+        axes[a.axis] = {
+          val: 0,
+          sum: 0
+        };
+      }
+
+      if (q.answer > 0) {
+        axes[a.axis].val += q.answer * a.value;
+      }
+      axes[a.axis].sum += Math.max(a.value, 0);
+    }
+
+    for (var j = 0; j < q.valuesNo.length; j++) {
+      var a = q.valuesNo[j];
+      if (!(a.axis in axes)) {
+        axes[a.axis] = {
+          val: 0,
+          sum: 0
+        };
+      }
+
+      if (q.answer < 0) {
+        axes[a.axis].val -= q.answer * a.value;
+      }
+      axes[a.axis].sum += Math.max(a.value, 0);
+    }
+  }
+
+  var url = "";
+  for (var aK in axes) {
+    if (axes[aK].val > 0) {
+      if (url != "") url += "&";
+      url += aK + "=" + calc_score(axes[aK].val, axes[aK].sum);
+    }
+  }
+
+  base64_url = window.btoa(url);
+  quiz_time = end_time();
+  $.post("save",{"data": base64_url, "time": quiz_time})
+    .always(function() {
+      url = "./results?" + base64_url;
+      console.log("It tooks " + quiz_time + " seconds");
+      location.href = url;
+    });
+}
